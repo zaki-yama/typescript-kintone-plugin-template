8059148cf61b027b2071e550479b50a26115b084
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-08-13 05:12:18 +0900

    Delete index.stories.tsx

--- src/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { storiesOf } from "@storybook/react";
-
-const Button: React.FC = ({ children }) => <div>{children}</div>;
-
-storiesOf("Button", module).add("with text", () => <Button>hello</Button>);
